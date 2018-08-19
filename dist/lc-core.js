395d17b0a72d5cb25f2dcd03d1df36c0d36e0987
Guillaume Le Cousin <guillaume@lecousin.net>
2018-08-19 12:23:20 +0200

    polyfill

--- dist/lc-core.js
+++ dist/lc-core.js
@@ -1,3 +1,39 @@
+if (!String.prototype.startsWith) {
+	String.prototype.startsWith = function(search, pos) {
+		return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+	};
+}
+
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
+
+
+Array.prototype.contains=function(e){return this.indexOf(e) != -1;};
+Array.prototype.remove=function(e){for(var i=0;i<this.length;++i)if(this[i]==e){this.splice(i,1);i--;};};
+Array.prototype.removeUnique=function(e){var i=this.indexOf(e);if(i>=0)this.splice(i,1);};
+Array.prototype.isSame = function(a) {
+	if (!Array.isArray(a)) return false;
+	if (this.length != a.length) return false;
+	for (var i = 0; i < this.length; ++i)
+		if (this[i] !== a[i])
+			return false;
+	return true;
+};
+Array.prototype.clear = function() { if (this.length > 0) this.splice(0,this.length); };
+Array.prototype.removeElements = function(toRemove) {
+	for (var i = 0; i < this.length; ++i) {
+		if (toRemove.contains(this[i])) {
+			this.splice(i,1);
+			i--;
+		}
+	}
+};
 if (typeof lc === 'undefined') lc = {};
 
 lc.core = {
@@ -1480,42 +1516,6 @@ lc.core.createClass("lc.log.FormatComponentMessage", function(size) {
 });
 
 lc.log.setFormat("${time} ${level} ${logger:15} ${message}");
-if (!String.prototype.startsWith) {
-	String.prototype.startsWith = function(search, pos) {
-		return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
-	};
-}
-
-if (!String.prototype.endsWith) {
-	String.prototype.endsWith = function(search, this_len) {
-		if (this_len === undefined || this_len > this.length) {
-			this_len = this.length;
-		}
-		return this.substring(this_len - search.length, this_len) === search;
-	};
-}
-
-
-Array.prototype.contains=function(e){return this.indexOf(e) != -1;};
-Array.prototype.remove=function(e){for(var i=0;i<this.length;++i)if(this[i]==e){this.splice(i,1);i--;};};
-Array.prototype.removeUnique=function(e){var i=this.indexOf(e);if(i>=0)this.splice(i,1);};
-Array.prototype.isSame = function(a) {
-	if (!Array.isArray(a)) return false;
-	if (this.length != a.length) return false;
-	for (var i = 0; i < this.length; ++i)
-		if (this[i] !== a[i])
-			return false;
-	return true;
-};
-Array.prototype.clear = function() { if (this.length > 0) this.splice(0,this.length); };
-Array.prototype.removeElements = function(toRemove) {
-	for (var i = 0; i < this.length; ++i) {
-		if (toRemove.contains(this[i])) {
-			this.splice(i,1);
-			i--;
-		}
-	}
-};
 lc.app.onDefined(["lc.resources", "lc.Cache"], function() {
 	
 	lc.resources._css_cache = new lc.Cache();
