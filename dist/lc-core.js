539d99a4ee970c85c314d8b73a0f53e66f57e6cc
Guillaume Le Cousin <guillaume@lecousin.net>
2018-10-14 12:37:30 +0200

    fix lc.http.rest status handlers

--- dist/lc-core.js
+++ dist/lc-core.js
@@ -2188,7 +2188,7 @@ lc.core.namespace("lc.http.rest", {
 	
 	handleResponseStatus: function(req, result) {
 		if (req.status == 200) return false;
-		if (typeof lc.http.rest.statusHandlers[req.status] !== undefined) {
+		if (typeof lc.http.rest.statusHandlers[req.status] !== 'undefined') {
 			lc.async.Callback.callListeners(lc.http.rest.statusHandlers[req.status], [req, result]);
 			return true;
 		}
